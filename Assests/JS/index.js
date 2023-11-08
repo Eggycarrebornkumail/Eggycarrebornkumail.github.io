ed823db0dcd244723e6417074fdd24d8953c3f19
Eggycarrebornkumail <150016206+Eggycarrebornkumail@users.noreply.github.com>
2023-11-08 11:32:18 -0800

    Update index.js

--- Assests/JS/index.js
+++ Assests/JS/index.js
@@ -1,20 +1,3 @@
 var says = [
   "Kumails updated website",
 ];
-
-function splashText() {
-  document.querySelector(".Index-SplashText").innerHTML =
-    says[Math.floor(Math.random() * says.length)];
-}
-
-document.addEventListener("DOMContentLoaded", (event) => {
-  splashText();
-
-  fetch("httpip.com/json")
-    .then((response) => response.json())
-    .then((data) => {
-      ipAddress = data.YourFuckingIPAddress;
-      says.push(`umm your ip: fafafaf;
-      splashText();
-    });
-});
